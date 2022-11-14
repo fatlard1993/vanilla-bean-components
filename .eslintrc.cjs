6d677ba3c7cc94029306a6b4383f8d584035f080
justfatlard <fatlard1993@gmail.com>
2022-11-13 16:40:07 -0800

    cleanup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,7 @@ module.exports = {
 		sourceType: 'module',
 		ecmaVersion: 2020,
 	},
-	extends: ['eslint:recommended', 'plugin:prettier/recommended', 'plugin:cypress/recommended'],
+	extends: ['eslint:recommended', 'plugin:prettier/recommended'],
 	rules: {
 		'no-async-promise-executor': 'off',
 		'no-prototype-builtins': 'off',
