a6979fd1d6aeb9001627c822b75fcbf7011ef381
justfatlard <fatlard1993@gmail.com>
2023-02-15 15:06:49 -0800

    cleanup

--- components/Button/test.js
+++ components/Button/test.js
@@ -6,7 +6,7 @@ import Button from '.';
 const container = new JSDOM().window.document.body;
 
 describe('Button', () => {
-	test('must render', async () => {
+	test('must render a button', async () => {
 		const name = 'test:button';
 
 		new Button({ textContent: name, appendTo: container });
--- demo/index.js
+++ demo/index.js
@@ -1,5 +1,8 @@
+import process from 'process';
 import { Page } from '../';
 
 import DemoRouter from './DemoRouter';
+
+window.process = process;
 
 new Page({ appendTo: document.getElementById('app'), appendChild: new DemoRouter() });
