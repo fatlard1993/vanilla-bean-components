36f7d0bc2f35e3cd322912895cbe1a0e3fe6af7c
justfatlard <fatlard1993@gmail.com>
2023-05-29 11:53:13 -0700

    add onSearch event

--- components/Search/Search.js
+++ components/Search/Search.js
@@ -8,6 +8,18 @@ export class Search extends Input {
 			...options,
 		});
 	}
+
+	onSearch(cb) {
+		const wrappedCb = evt => {
+			evt.value = evt.target.value || this.elem.value || this.value;
+
+			cb(evt);
+		};
+
+		this.elem.addEventListener('search', wrappedCb);
+
+		return () => this.elem.removeEventListener('search', wrappedCb);
+	}
 }
 
 export default Search;
