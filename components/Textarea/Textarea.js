f13391feeb370f54887cda2800b9a484deff870f
justfatlard <fatlard1993@gmail.com>
2023-11-13 20:42:12 -0800

    fix textarea auto height

--- components/Textarea/Textarea.js
+++ components/Textarea/Textarea.js
@@ -12,19 +12,19 @@ class Textarea extends Input {
 	setOption(key, value) {
 		if (key === 'height') {
 			if (value === 'auto') {
-				this.updateAutoHeight();
+				this.__updateAutoHeight = () => {
+					this.elem.style.height = `${(this.elem.value?.match(/\n/g) || '').length + 2}em`;
+				};
 
-				this.elem.addEventListener('input', this.updateAutoHeight);
+				this.__updateAutoHeight();
+
+				this.elem.addEventListener('input', this.__updateAutoHeight);
 			} else {
-				this.elem.removeEventListener('input', this.updateAutoHeight);
+				this.elem.removeEventListener('input', this.__updateAutoHeight);
 
 				this.elem.style.height = typeof value === 'number' ? `${value + 1}em` : value;
 			}
 		} else super.setOption(key, value);
-	}
-
-	updateAutoHeight() {
-		this.elem.style.height = `${(this.elem.value?.match(/\n/g) || '').length + 2}em`;
 	}
 }
 
