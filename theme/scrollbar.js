5bb9bae912a2079c211432596b4cf43a47c92f1b
justfatlard <fatlard1993@gmail.com>
2025-01-26 20:19:18 -0800

    improved scrollbar styles

--- theme/scrollbar.js
+++ theme/scrollbar.js
@@ -4,20 +4,23 @@ export default `
 	::-webkit-scrollbar {
 		width: 24px;
 		height: 24px;
+		background: ${colors.white.setAlpha(0.06)};
 	}
 	::-webkit-scrollbar-button {
 		width: 6px;
 		height: 6px;
 	}
 	::-webkit-scrollbar-thumb {
-		background: ${colors.darkest(colors.gray)};
+		background: ${colors.white.setAlpha(0.06)};
 		-webkit-box-shadow: inset 0 0 2px 1px ${colors.blue};
 		border-radius: 12px;
 	}
 	::-webkit-scrollbar-thumb:hover {
+		background: ${colors.white.setAlpha(0.09)};
 		-webkit-box-shadow: inset 0 0 3px 1px ${colors.blue};
 	}
 	::-webkit-scrollbar-thumb:active {
+		background: ${colors.white.setAlpha(0.12)};
 		-webkit-box-shadow: inset 0 0 4px 2px ${colors.blue};
 	}
 `;
