6d3cfa3243451e559f795e64600b003d74c4964d
justfatlard <fatlard1993@gmail.com>
2025-02-25 22:20:02 -0800

    improved demo menu

--- demo/DemoMenu.js
+++ demo/DemoMenu.js
@@ -14,70 +14,18 @@ const StyledComponent = styled(
 		flex-wrap: wrap;
 		padding: 12px 24px;
 		background-color: ${colors.darkest(colors.gray)};
-
-		&.collapsed {
-			height: 36px;
-			overflow: hidden;
-
-			h2 {
-				opacity: 0;
-			}
-		}
 	`,
 );
 
 export default class DemoMenu extends StyledComponent {
-	constructor(options = {}) {
-		super({
-			collapsed: document.body.clientWidth < 780,
-			...options,
-		});
-	}
-
 	render() {
 		this._links = [];
-
-		const menuItems = {
-			documentation: [],
-			componentDemos: [],
-			examples: [],
-		};
-
-		Object.keys(views).forEach(name => {
-			const href = `#${name}`;
-			let parent = 'componentDemos';
-
-			if (name.startsWith('/examples/')) parent = 'examples';
-			else if (name.startsWith('/documentation/')) parent = 'documentation';
-
-			menuItems[parent].push({
-				ListItemComponent: Link,
-				listItemOptions: {
-					style: {
-						border: 'none',
-						padding: '0',
-						paddingLeft: '18px',
-					},
-				},
-				textContent: /^\/\w+\//.test(name) ? name.replace(/^\/\w+\//, '') : name.slice(1),
-				href,
-				styles: ({ colors }) => ({
-					textDecoration: 'none',
-					color: 'inherit',
-					padding: '6px 6px 9px 6px',
-					borderBottom: '1px solid #999',
-					display: 'block',
-					...(window.location.hash === href ? { background: colors.black, cursor: 'default' } : {}),
-				}),
-			});
-		});
 
 		this.menuPopover = new Popover({ appendTo: this, style: { maxHeight: '60%' } });
 
 		this.menu = new Menu({
-			items: Object.keys(menuItems).map(item => capitalize(fromCamelCase(item, ' '), true)),
 			onSelect: ({ target }) => {
-				this.subMenu.options.items = menuItems[toCamelCase(target.textContent)];
+				this.subMenu.options.items = this.menu.options.menuItems[toCamelCase(target.textContent)];
 
 				this.menu.children.forEach(item => {
 					item.elem.style.backgroundColor = 'inherit';
@@ -105,6 +53,44 @@ export default class DemoMenu extends StyledComponent {
 			appendTo: this,
 			popovertarget: this.menuPopover.uniqueId,
 			onPointerPress: ({ clientX, clientY }) => {
+				const menuItems = {
+					documentation: [],
+					componentDemos: [],
+					examples: [],
+				};
+
+				Object.keys(views).forEach(name => {
+					const href = `#${name}`;
+					let parent = 'componentDemos';
+
+					if (name.startsWith('/examples/')) parent = 'examples';
+					else if (name.startsWith('/documentation/')) parent = 'documentation';
+
+					menuItems[parent].push({
+						ListItemComponent: Link,
+						listItemOptions: {
+							style: {
+								border: 'none',
+								padding: '0',
+								paddingLeft: '18px',
+							},
+						},
+						textContent: /^\/\w+\//.test(name) ? name.replace(/^\/\w+\//, '') : name.slice(1),
+						href,
+						styles: ({ colors }) => ({
+							textDecoration: 'none',
+							color: 'inherit',
+							padding: '6px 6px 9px 6px',
+							borderBottom: '1px solid #999',
+							display: 'block',
+							...(window.location.hash === href ? { background: colors.black, cursor: 'default' } : {}),
+						}),
+					});
+				});
+
+				this.menu.options.menuItems = menuItems;
+				this.menu.options.items = Object.keys(menuItems).map(item => capitalize(fromCamelCase(item, ' '), true));
+
 				this.menuPopover.setStyle({
 					top: `${clientY + 6}px`,
 					left: `${clientX + 6}px`,
@@ -122,7 +108,8 @@ export default class DemoMenu extends StyledComponent {
 		window.addEventListener('hashchange', () => {
 			title.elem.textContent = window.location.hash.slice(2);
 
-			this.menuPopover.close();
+			this.menuPopover.hide();
+			this.subMenu.options.items = [];
 		});
 
 		super.render();
