fca3a9e395965bd333f89a2b7ce5e13d00a19309
justfatlard <fatlard1993@gmail.com>
2025-04-16 18:22:34 -0700

    fix randInt not including max

--- utils/rand.js
+++ utils/rand.js
@@ -1,8 +1,12 @@
-/** @type {(min: number, max: number) => number} Generate a random number from a range (inclusive) */
+/** @type {(min: number, max: number) => number} Generate a random number from a range (min inclusive, max exclusive) */
 export const rand = (min = -999, max = 999) => Math.random() * (max - min) + min;
 
-/** @type {(min: number, max: number) => number} Generate a random integer from a range (inclusive) */
-export const randInt = (min, max) => Number.parseInt(rand(min, max));
+/** @type {(min: number, max: number) => number} Generate a random integer from a range (min & max inclusive) */
+export const randInt = (min = -999, max = 999) => {
+	min = Math.ceil(min);
+
+	return Math.floor(Math.random() * (Math.floor(max) - min + 1) + min);
+};
 
 /** @type {(array: Array) => number} Choose a random item from an array */
 export const randFromArray = array => array[randInt(0, array.length)];
